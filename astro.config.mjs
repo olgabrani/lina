73737f8dceb5b6ad035e15326214b57d325345f4
Olga Brani <olgabrani@grnet.gr>
2025-08-27 16:17:00 +0300

    Fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://olgabrani.github.io/',
+  site: 'https://olgabrani.github.io',
   base: '/lina',
 });
